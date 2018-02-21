7518b11b86287d2325963563f18cffcc5cf5f915
itan <itan@itans-Mac.local>
2018-02-21 13:26:55 -0800

    added javacript class notes

---
+++ js/scripts.js
@@ -0,0 +1,69 @@
+//This is a comment
+/*
+anything here is a comment
+comment syntex
+*/
+
+var name = 100;
+var n = 20;
+
+
+console.log(name+n);/* it will sent anything in () to the browser
+make sure your root folder have the html file that you want to sent to with the right script inside the html file <script src="js/scripts.js"></script> */
+
+
+alert (name) // this gives you a pop up window in your browser on with value (name)
+var sum1 = "100"+"100" ; //gives 100100
+var sum2 = 100+100; //gives 200
+
+/* not valid: True, TRUE, trUe, etc
+ true, falso
+*/
+
+var go = true;
+var katyage = 44;
+if (katyage > 21) {
+  console.log("take a drink");
+}else {
+  console.log("you are too young");
+}
+if (katyage != 21) {
+  console.log("cant drink");
+}else {
+  console.log("you can drink");
+}
+/*
+== : comparison
+=== : Type comparison
+>= or >==: equal to or greater
+<= or <==: equal to or smaller
+!= or !==: should not be the same as left side
+*/
+console.log(sum1,sum2);
+console.log(go);
+console.log(false);
+
+// week 3 day 2
+var count = 0;
+
+while (count < 10) {
+  //count = count+ 1; //or count ++;
+  count++;
+  console.log("THe current loop is " + count);
+  console.log(sum1, sum2);
+
+
+}
+
+
+var firstline = "*";
+while (count<20) {
+  console.log(firstline);
+  firstline = firstline + "+"
+  count++;
+
+  if (count%2 === 1) {
+      firstline= firstline + " "
+  }
+
+}
---
+++ js/scripts2.js
@@ -0,0 +1,46 @@
+// var count =0;
+//
+// // while (count<10) {
+// //   count=count + 1;
+// // }
+//
+// for (var count = 0; count < 10; count++) {
+//
+//   console.log("hello");
+// }
+//
+// do {
+//   console.log("True");
+// } while (false); // will run at least once in case you need to run at least once unlke for or while loop
+
+
+// 2 numbers 2 strings 2 boolean
+var a = 1;
+var b = 2;
+var nameA = "Adam";
+var nameB = "Jane";
+var power = false;
+var c1 =1 , c2 = 3, c3 = 5; // can be written using , on a line instead of ;
+var myFirstArray = ["sunday","Monday","Tuesday ","Wed"];
+// while (a<=20) {
+//   a++
+//   console.log(a);
+// }
+
+// for (var a = 1; a<= 20; a++) {
+//   console.log("goodbye");
+//   for (var a = 0; a < 5; a++) {
+//     console.log("inner loop");
+//   }
+// }
+
+myFirstArray[3] = " Doesnt exist! "//change existing data
+var currentDay = myFirstArray[3]; //pull data from array
+  for (var i = 0; i < 3; i++) {
+    console.log(myFirstArray[i]);
+  }
+
+var myarray = ["no1","no2","no3","4","haha"];
+  for (var i = 0; i < myarray.length; i++) {
+    console.log(myarray[i]);
+  }
