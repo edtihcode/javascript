62ff868aa77e03a2f4e2332633e6011029b7b01b
itan <itan@itans-Mac.local>
2018-02-22 12:20:07 -0800

    added notes for functions

--- js/practicescript.js
+++ js/practicescript.js
@@ -1,8 +1,20 @@
+//
+// function myFirstFunction(text, number) {
+//     console.log(text);
+//     console.log(number);
+// }
+//
+// myFirstFunction("asd");
+// myFirstFunction("dasdd");
+// myFirstFunction("asd",2321);
+//
 
+function myFirstFunction(a, b) {
+    var sum = a + b;
+    var mul = a * b;
+    console.log("addition for " + a + "+" + b +" is "+  sum);
+    console.log("multiple for " + a + "x" + b +" is "+  mul);
+}
 
-var a = 1;
-var b = 2;
-var c = 3;
-var d = 4;
-console.log(a);
-alert (b)
+myFirstFunction(1,2);
+myFirstFunction(4,2);
