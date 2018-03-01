b5be1c2491a25fa65091ee8d203f7e48c142b333
itan <itan@itans-Mac.local>
2018-03-01 00:46:35 -0800

    added changes

---
+++ syntexCheetSheet.js
@@ -0,0 +1,7 @@
+
+
+//insertAdjacentHTML add content to DOM with content
+document.getElementById("text").insertAdjacentHTML('beforeend', contentTobeAdded)
+//&nbsp is the code you put in htlm for spaces as html will take away spaces eg. "hello "&nbsp
+
+names.push()// add to array
